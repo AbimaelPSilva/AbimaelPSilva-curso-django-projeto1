e16d1eef3f0a9178109408e607dbcd2146566be3
Abimael P Silva <abimael.bkp@gmail.com>
2022-05-29 09:43:25 -0300

    Arquivos CSS

--- recipes/views.py
+++ recipes/views.py
@@ -1,16 +1,7 @@
 import imp
 
-from django.http import HttpResponse
 from django.shortcuts import render  # Alterado
 
 
 def home(request):
-    return render(request, 'recipes/home.html')  # alterado
-
-
-def contato(request):
-    return HttpResponse('Contato')
-
-
-def sobre(request):
-    return HttpResponse('Sobre')
+    return render(request, 'recipes/pages/home.html')  # alterado
