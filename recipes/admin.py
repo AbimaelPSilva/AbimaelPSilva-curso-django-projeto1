82324ef03d738fa2756d47bb382926d13f2fa058
Abimael P Silva <abimael.bkp@gmail.com>
2022-06-07 09:53:18 -0300

    07_06_2022

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Category, Recipe
+
+
+# Existem duas formas de registrar
+# 1ª Forma
+class CategoryAdmin(admin.ModelAdmin):
+    ...
+
+
+admin.site.register(Category, CategoryAdmin)
+
+# 2ª Forma
+
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    ...
